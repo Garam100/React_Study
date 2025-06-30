6b152cf79e294a514b547983f60841fda92d714e
Garam100 <backgaram123@gmail.com>
2025-06-30 11:43:14 +0900

    check박스 포함된 투두리스트 생성

--- todolist/src/TodoList.tsx
+++ todolist/src/TodoList.tsx
@@ -1,13 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 
-interface MyComponentProps{
-    title : string;
+
+type Todo = {
+    id : number;
+    text : string;
+    isChecked : boolean;
 }
 
-const TodoList : React.FC<MyComponentProps>=({title}) =>{
+
+const TodoList : React.FC = ()  =>{
+    const [todos, setTodos ] = useState<Todo[]>([{id: 1, text: "밥먹기", isChecked : true}, {id: 2, text: "오후 근로하기", isChecked : false}, {id: 3, text: "청소하기", isChecked : true}]);
+
+
     return(
-        <div className='container'>
-            <h1>{title}</h1>
+        <div>
+            <h1 className='titlebk'>오늘 할일</h1>
+            <div className='container'>
+                <div className='board'>
+                    <ul>
+                        <li>{todos[0].text}</li>
+                        <li>{todos[1].text}</li>
+                        <li>{todos[2].text}</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
