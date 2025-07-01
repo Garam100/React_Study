fc29f16e8ade2ee9cf95177e1508da2de5e338a3
Garam100 <backgaram123@gmail.com>
2025-07-01 11:08:45 +0900

    추가하기 생성

--- todolist/src/TodoList.tsx
+++ todolist/src/TodoList.tsx
@@ -11,16 +11,29 @@ type Todo = {
 
 const TodoList : React.FC = ()  =>{
     const [todos, setTodos ] = useState<Todo[]>([{id: 1, text: "밥먹기", isChecked : true}, {id: 2, text: "오후 근로하기", isChecked : false}, {id: 3, text: "청소하기", isChecked : true}]);
+    const [newTodo, setNewTodo] = useState<string>('');
 
     const handleCheckboxChange = (itemId : number) =>{
         setTodos((prevItems)=>prevItems.map((item)=>
             item.id === itemId ? {...item, isChecked : !item.isChecked} : item))
 
     }
+    const addTodo = () => {
+        if(newTodo.trim() !==''){
+            setTodos([...todos, {id: Date.now(), text : newTodo, isChecked: false}]);
+            setNewTodo('');
+        }
+    }
 
     return(
         <div className='container mt-5'>
             <h1 className='titlebk'>오늘 할일</h1>
+
+            <div className='form-group d-flex align-items-center mb-3'>
+                <input type='text' className='form-control me-2' placeholder = "할일 입력" onChange = {(e)=> setNewTodo(e.target.value)} value = {newTodo} style={{writingMode: 'horizontal-tb'}}/>
+                <button className = 'btn btn-success' onClick = {addTodo}style = {{whiteSpace : 'nowrap'}}>추가하기</button>
+            </div>
+
             <div className='card'>
                 <div className='card-body'>
                     <ul className='list-group'>
