82aeccc139501e28df353da086213213d2a1dc37
Garam100 <backgaram123@gmail.com>
2025-07-02 11:44:44 +0900

    네비바 추가, 메인 이미지 1추가

--- bookstore/src/layout/Content.tsx
+++ bookstore/src/layout/Content.tsx
@@ -1,8 +1,10 @@
+import book1 from '../imeages/book1.jpg';
+
 const Content : React.FC = () =>{
 
     return(
         <div>
-            본문
+            <img className='d-block w-100' src={book1} alt='1퍼센트 부자의 법칙'></img>
         </div>
     );
 }
--- bookstore/src/layout/Header.tsx
+++ bookstore/src/layout/Header.tsx
@@ -1,9 +1,20 @@
 import type React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const Header : React.FC = () =>{
 return(
     <div>
-        헤더
+        <Navbar style={{backgroundColor: '#8cd6e0'}} data-bs-theme = 'dark'>
+            <Container>
+                <Navbar.Brand href="/"><h1>Bookstore</h1></Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/about">About</Nav.Link>
+                    <Nav.Link href="/product">Product</Nav.Link>
+                    <Nav.Link href="/contact">Contact</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
     </div>
 );
 }
