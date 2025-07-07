3c993e77876563fcaea4af8086fb12b09c797666
Garam100 <backgaram123@gmail.com>
2025-07-07 10:18:28 +0900

    refactor: bookitem을 받아 card하나를 출력하는 함수를 분리하여 BookItem.tsx로 제작

--- bookstore/src/layout/Content.tsx
+++ bookstore/src/layout/Content.tsx
@@ -3,6 +3,7 @@ import MyCarousel from "../component/Carousel";
 import bookdata, { type Book } from "../data/Bookdata";
 import {Link} from 'react-router-dom';
 import { Card, Col, Container, Row, Button } from "react-bootstrap";
+import BookItem from "../component/BookItem";
 
 const Content : React.FC = () =>{
     const [books,setBook] = useState<Book[]>(bookdata);
@@ -19,31 +20,9 @@ const Content : React.FC = () =>{
             </div>
 
             <Row className="text-center">
-                {books.map((book)=>(
-                    <Col sm={6} md = {4} key={book.id} className="box p-4">
-                        <Card className="border rounded shadow-sm p-3" style={{height: '500px', width: '300px'}}>
-                            <Card.Body>
-                                <Card.Img 
-                                variant="top"
-                                src='/imeages/1.jpg'
-                                style={{height: '240px', width: '173px', objectFit:'cover'}}
-                                alt={book.title}/>
-                                <Card.Title>{book.title}</Card.Title>
-                                <div className="description">
-                                    <span className="author">작가: {book.author}</span>
-                                    <p></p>
-                                    <span className="star">별점: {book.star}</span>
-                                    <p></p>
-                                </div>
-                            </Card.Body>
-                            <div className="linkTo">
-                                <Link to="/">
-                                <Button variant = "primary">자세히 보기</Button>
-                                </Link>
-                            </div>
-                        </Card>
-                    </Col>
-                ))}
+                {books.map((book)=>{
+                    return <BookItem key={book.id} book={book}/>
+                })}
             </Row>
            </Container>
         </div>
